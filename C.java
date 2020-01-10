1cd2b1245e6b75af8cd78486e93df1240f4f5ea5
krishnahari123 <59728787+krishnahari123@users.noreply.github.com>
2020-01-10 17:29:42 +0530

    Update C.java

--- C.java
+++ C.java
@@ -4,7 +4,7 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("helloworld");
+		System.out.println("helloworld this is java ");
 
 	}
 
