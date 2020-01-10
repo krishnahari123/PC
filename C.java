7ef2dcc167c67e7f9a44a1acf31cdaec05c63125
krishnahari123 <59728787+krishnahari123@users.noreply.github.com>
2020-01-10 15:26:25 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,21 @@
+package com.enveelop.HK;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "A";
+		
+		String sr = "a";
+		
+		System.out.println(s.compareTo(sr));
+		
+		String ss = "Sold items (7)";
+		String ss1 = "(7)";
+		System.out.println(ss.substring(12,13));
+		System.out.println(ss1.substring(1, 2));
+
+	}
+
+}
